220c9290080f6d1e6f0c7ce9c415b5efeafc1fff
Nhat <nhatna17411c@st.uel.edu.vn>
2020-05-21 19:50:43 +0700

    Lam do Cart, sua DB

---
+++ UELWeb2Hasako/UELWeb2Hasako/Controllers/GiohangController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UELWeb2Hasako.Models;
+namespace UELWeb2Hasako.Controllers
+{
+    public class GiohangController : Controller
+    {
+        // GET: Giohang
+        //Tao doi tuong data chua du lieu tu Models
+        dbHasakoProjectDataContext data = new dbHasakoProjectDataContext();
+        public List<Giohang> Laygiohang()
+        {
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang == null)
+            {
+                //Neu gio hang chua ton tai thi khoi tao lstGiohang
+                lstGiohang = new List<Giohang>();
+                Session["Giohang"] = lstGiohang;
+            }
+            return lstGiohang;
+        }
+        //Them hang vao gio
+        public ActionResult ThemGiohang(int iMahaisan, string strURL)
+        {
+            //Lay session gio hang
+            List<Giohang> lstGiohang = Laygiohang();
+            //Kiem tra session da ton tai trong Session["Giohang"] chua?
+            Giohang sanpham = lstGiohang.Find(n => n.iMahaisan == iMahaisan);
+            if (sanpham == null)
+            {
+                sanpham = new Giohang(iMahaisan);
+                lstGiohang.Add(sanpham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanpham.iSoluong++;
+                return Redirect(strURL);
+            }
+        }
+        //Tong so luong
+        private int Tongsoluong()
+        {
+            int iTongsoluong = 0;
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang != null)
+            {
+                iTongsoluong = lstGiohang.Sum(n => n.iSoluong);
+            }
+            return iTongsoluong;
+        }
+
+        //Tomg tien
+        private double TongTien()
+        {
+            double iTongtien = 0;
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang != null)
+            {
+                iTongtien = lstGiohang.Sum(n => n.dThanhtien);
+            }
+            return iTongtien;
+        }
+        //Xay dung trang gio hang
+    
+        public ActionResult GioHang()
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            if (lstGiohang.Count == 0)
+            {
+                //return RedirectToAction("IndexHasako", "Hasako");
+            }
+            ViewBag.Tongsoluong = Tongsoluong();
+            ViewBag.TongTien = TongTien();
+            return View(lstGiohang);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UELWeb2Hasako/UELWeb2Hasako/Models/Giohang.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UELWeb2Hasako.Models;
+namespace UELWeb2Hasako.Models
+{
+    public class Giohang
+    {
+        dbHasakoProjectDataContext data = new dbHasakoProjectDataContext();
+        public int iMahaisan { get; set; }
+        public string sTenhaisan { get; set; }
+        public string sAnhbia { get; set; }
+        public Double dDongia { get; set; }
+        public int iSoluong { get; set; }
+        public Double dThanhtien
+        {
+            get { return iSoluong * dDongia; }
+        }
+        //Khoi tao theo ma hang duoc truyen vao voi so luong mac dinh la 1
+        public Giohang(int Mahaisan)
+        {
+            iMahaisan = Mahaisan;
+            HAISANKHO haisan = data.HAISANKHOs.Single(n => n.MaHS == iMahaisan);
+            sTenhaisan = haisan.TenHS;
+            sAnhbia = haisan.Anhbia;
+            dDongia = double.Parse(haisan.Dongia.ToString());
+            iSoluong = 1;
+
+        }
+    }
+}
