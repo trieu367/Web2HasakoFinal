bc94f3932bef202a68704c20b8411b46ba5a783e
trieu367 <trieu3678@gmail.com>
2020-05-22 21:55:59 +0700

    them layout trang admin

---
+++ UELWeb2Hasako/UELWeb2Hasako/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UELWeb2Hasako.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult IndexAdmin()
+        {
+            return View();
+        }
+        public ActionResult ThongKe()
+        {
+            return View();
+        }
+    }
+}
